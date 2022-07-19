740fc5d9de6cf80852c6439887f0cbfadaea90e5
justinnietzer <justinnietzer@gmail.com>
2022-07-19 00:55:48 -0400

    Added Splash Screen

---
+++ app/src/main/java/com/example/njitcarpoolapp/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.njitcarpoolapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        //Making the splash screen fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splash_activity);
+
+
+        //Only show Splash Screen for a certain amount of time
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
