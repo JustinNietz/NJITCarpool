763787754bee9599bb96d0108ecf89758d58c1ba
justinnietzer <justinnietzer@gmail.com>
2022-07-27 05:36:30 -0400

    Adding login and registration 1

--- app/src/main/java/com/example/njitcarpoolapp/MainActivity.java
+++ app/src/main/java/com/example/njitcarpoolapp/MainActivity.java
@@ -1,35 +1,31 @@
 package com.example.njitcarpoolapp;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.WindowManager;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
-import com.firebase.ui.auth.AuthMethodPickerLayout;
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
-
-import javax.annotation.Nullable;
-
-public class DriverAuthActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {
+    private Button nDriver, nCustomer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.driverauth_activity);
+        setContentView(R.layout.main_activity);
+
+        nDriver = (Button) findViewById(R.id.driver);
+        nCustomer = (Button) findViewById(R.id.customer);
+
+        nDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 
 
